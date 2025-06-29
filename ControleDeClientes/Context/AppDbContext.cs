61d8fa230f80226b99f3c811a475b220580d8d4d
Pedro Yan <pedroyyandias@gmail.com>
2025-06-29 10:37:37 -0300

    refactor(db): migração do banco de dados PostgreeSQL para o SQLite

--- ControleDeClientes/Context/AppDbContext.cs
+++ ControleDeClientes/Context/AppDbContext.cs
@@ -1,28 +1,17 @@
 ﻿using ControleDeClientes.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace ControleDeClientes.Context {
     public class AppDbContext : DbContext {
         public DbSet<EntityCliente> Clientes { get; set; }
 
-        // Criando a connection string
-        private readonly string _connectionString;
-
-        // Lendo o Json com o caminho para o banco
-        public AppDbContext() {
-            var configuration = new ConfigurationBuilder()
-                .SetBasePath(Directory.GetCurrentDirectory())
-                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-                .Build();
-
-            // Armazenando o caminho do Json dentro da connection string
-            _connectionString = configuration.GetConnectionString("DefaultConnection");
-        }
-
         // Conectando com o banco usando connection string
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
-            optionsBuilder.UseNpgsql(_connectionString);
+            var dbPath = Path.Combine(AppContext.BaseDirectory, "clientes.db");
+            optionsBuilder.UseSqlite($"Data Source={dbPath}");
+
         }
     }
 }
